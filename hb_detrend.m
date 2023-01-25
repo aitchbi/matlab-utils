dcaf562b2f612aa0ec2f5c496f62f155b74b6334
hbehjat <hamid.behjat@gmail.com>
2023-01-25 12:05:27 +0100

    Extended to detrend higher order polynomials

--- hb_detrend.m
+++ hb_detrend.m
@@ -1,15 +1,24 @@
-function X = hb_detrend(X,n,rmConstant,I)
+function X = hb_detrend(X,n,polyords,I)
 %HB_DETREND detrends each row (n=1) or column (n=2) of matrix X, chunk by
 % chunk if too many rows or columns, respectively. 
-% 
-% I: inclusion list; logical array, specifying which row/columns to
-% do/skip. 
+%
+% Inputs:
+%   X: matrix.
+%   n: 1 (row) or 2 (column).
+%   polyords: a non negative integer scalar or vector; polynomial orders.
+%   I: inclusion list; logical array, specifying which row/columns to
+%   do/skip.
+%
+% Outputs: 
+%   X: detrended matrix.
+%
+% Examples:
+%   X = hb_detrend(X,1,1);     % each row: remove linear trend.
+%   X = hb_detrend(X,1,[0 1]); % each row: remove mean & linear trend.
+%   X = hb_detrend(X,1,[1 2]); % each row: remove linear & quad trends.
 %
 % Hamid Behjat
 
-if ~exist('rmConstant','var') || isempty(rmConstant)
-    rmConstant = false;
-end
 if ~exist('I','var') || isempty(I)
     I = true(1,size(X,n));
 end
@@ -25,18 +34,18 @@ end
 Xskip = X(:,not(I));
 X = X(:,I);
 
-N = size(X,2); 
-L = 1e5; 
-K = ceil(N/L); 
+N = size(X,2);
+L = 1e5;
+K = ceil(N/L);
 for k=1:K
     if k~=K
         kk = (k-1)*L+1:k*L;
     else
         kk = (k-1)*L+1:N;
     end
-    X(:,kk) = detrend(X(:,kk));
-    if rmConstant
-        X(:,kk) = detrend(X(:,kk),'constant');
+    for iOrd=1:length(polyords)
+        ord = polyords(iOrd);
+        X(:,kk) = detrend(X(:,kk),ord);
     end
 end
 
