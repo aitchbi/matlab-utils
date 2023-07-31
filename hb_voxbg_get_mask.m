4a46c5e5c70c9a28ce79653d74600cddcf4d03e7
hbehjat <hamid.behjat@gmail.com>
2023-07-31 11:10:41 +0100

    Update hb_voxbg_get_mask.m
    -option to input loaded G structure instead of file path.
    -option to prevent overwrite existing file.
    -option to specify output file name.

--- hb_voxbg_get_mask.m
+++ hb_voxbg_get_mask.m
@@ -1,14 +1,15 @@
-function f_mask = hb_voxbg_get_mask(f_graph)
+function f_mask = hb_voxbg_get_mask(graph,varargin)
 % HB_VOXBG_GET_MASK extracts 3D graph mask from a voxBG and writes it as a
 % nifti file in teh same directory as the graph.
 %
 % Inputs:
-%   f_g: voxel-wise brain graph; .mat file. 
+%   graph: voxel-wise brain graph; graph structure or absolute address to
+%   graph structure save as .mat file.
 %
 %  Outputs:
 %   f_m: nifit binary mask file, showing indices in 3D volume corresponding
-%   to graph vertices. The file is written in the same folder as f_g, and
-%   the name will be the same as f_g appended with '_mask'.
+%   to graph vertices. The file is written in the same folder as the graph,
+%   and the name will be the same as f_g appended with '_mask'.
 %
 % Examples:
 % f = hb_voxbg_get_mask('/a/b/c/G.gmlh.res1250.spaceT1w.mat');
@@ -18,12 +19,35 @@ function f_mask = hb_voxbg_get_mask(f_graph)
 %
 % Hamid Behjat
 
-assert(endsWith(f_graph,'.mat'));
+p = inputParser;
+addParameter(p,'Overwrite',true);
+addParameter(p,'OutputFile',[]); 
+parse(p,varargin{:});
+opts = p.Results;
 
-f_mask = strrep(f_graph,'.mat','_mask.nii');
+if ischar(graph)
+    assert(endsWith(graph,'.mat'));
+    f_graph = graph;
+    d = load(f_graph);
+    G = d.G;
+elseif isstruct(graph)
+    G = graph;
+    f_graph = G.f.graph;
+end
+clear graph;
 
-d = load(f_graph);
-G = d.G;
+if isempty(opts.OutputFile)
+    f_mask = strrep(f_graph,'.mat','_mask.nii');
+else
+    f_mask = opts.OutputFile;
+end
+
+if not(opts.Overwrite)
+    if exist(f_mask,'file')
+        fprintf('\nGraph mask already exists.\n');
+        return;
+    end
+end
 
 h = struct;
 h.dim = G.dim;
