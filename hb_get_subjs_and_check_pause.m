ef4e2d4c9386daf0a29acdb80da15fe895a18de7
hbehjat <hamid.behjat@gmail.com>
2025-10-15 09:19:36 +0200

    Update hb_get_subjs_and_check_pause.m

--- hb_get_subjs_and_check_pause.m
+++ hb_get_subjs_and_check_pause.m
@@ -1,4 +1,4 @@
-function [I, PauseLength] = hb_get_subjs_and_check_pause(n_file, RunPause)
+function [I, PauseTime] = hb_get_subjs_and_check_pause(n_file, RunPause)
 
 % Inputs:
 %   n_file: file name of a script that is to be run, without .m extension. 
@@ -9,9 +9,9 @@ function [I, PauseLength] = hb_get_subjs_and_check_pause(n_file, RunPause)
 % 
 % *** Expected format of n_file:
 %
-% option 1: '*_SubjsFrom<X>To<Y>'
+% option 1: '*_Fr<X>To<Y>'
 %
-% option 2: '*_SubjsFrom<X>To<Y>_Pause<T1><T2>'
+% option 2: '*_Fr<X>To<Y>_Pause<T1><T2>'
 %
 %  <X>: an integer; first subject number 
 %  <Y>: an integer; last subject number
@@ -28,40 +28,40 @@ if not(exist('RunPause','var')) || isempty(RunPause)
     RunPause = false;
 end
 
-d1 = strfind(n_file, 'From') + length('From');
+d1 = strfind(n_file, 'Fr') + length('Fr');
 d2 = strfind(n_file, 'To');
 d3 = d2 + length('To');
-iSta = str2double(n_file(d1:d2-1));
+iFr = str2double(n_file(d1:d2-1));
 
 if contains(n_file, 'Pause')
     iPause = strfind(n_file, 'Pause') + length('Pause');
     if contains(n_file, 'Hours')
         iHours = strfind(n_file, 'Hours');
-        PauseLength = str2double(n_file(iPause:iHours-1));
-        PauseLength = PauseLength*60*60; % hours to seconds
+        PauseTime = str2double(n_file(iPause:iHours-1));
+        PauseTime = PauseTime*60*60; % hours to seconds
     else
         assert(contains(n_file, 'Minutes'),...
             'Pause should be give in either Hours or Minutes.');
         iMinutes = strfind(n_file, 'Minutes');
-        PauseLength = str2double(n_file(iPause:iMinutes-1));
-        PauseLength = PauseLength*60; % minutes to seconds
+        PauseTime = str2double(n_file(iPause:iMinutes-1));
+        PauseTime = PauseTime*60; % minutes to seconds
     end
-    iEnd = str2double(n_file(d3:strfind(n_file, '_Pause')-1));
+    iTo = str2double(n_file(d3:strfind(n_file, '_Pause')-1));
 else
-    PauseLength = 0;
-    iEnd = str2double(n_file(d3:end));
+    PauseTime = 0;
+    iTo = str2double(n_file(d3:end));
 end
 
-if iEnd>=iSta
-    I = iSta:iEnd;
+if iTo>=iFr
+    I = iFr:iTo;
 else
-    I = iEnd:-1:iSta;
+    I = iTo:-1:iFr;
 end
 
 if RunPause
-    if PauseLength~=0
-        fprintf('\n.Script paused for %d seconds at %s. \n', PauseLength, datetime);
-        pause(PauseLength);
+    if PauseTime~=0
+        fprintf('\n.Script paused for %d seconds at %s. \n', PauseTime, datetime);
+        pause(PauseTime);
     end
 end
 end
