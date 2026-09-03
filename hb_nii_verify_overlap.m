1880a22739be954a46e449375f8da4aebef2ff7f
hbehjat <hamid.behjat@gmail.com>
2026-09-03 11:29:18 +0200

    Update hb_nii_verify_overlap.m

--- hb_nii_verify_overlap.m
+++ hb_nii_verify_overlap.m
@@ -3,42 +3,46 @@ function [sts, msg, p1, p2] = hb_nii_verify_overlap(f1,f2,p,type,varargin)
 % images of the same subject after being registered and resliced to match
 % voxel-by-voxel. 
 %
-% Inputs:
+% inputs:
 %   f1: path of file 1.
 %
 %   f2: path of file 2.
 %
 %   p: a scalar; overlap percentage in (1 100].
 %
-%   type: 'both' or 'either' or 'second'; should both images at least have
-%   95% overlap or just at least one of them? For a fixed p, 'both' results
-%   in a stricter check.
+%   type: 'both' or 'either' or 'second'; should 'both' images at least
+%   have p% overlap, or 'either', or just the 'second'? most stringent
+%   check is for 'both', however, that may not be suitable eg if one image
+%   is brain masked whereas the other one is not. 
 %
-% Outputs:
+% outputs:
 %   sts: logicical; verification passed?
 %
 %   p1: percentage of overlap for f1
 %
 %   p2: percentage of overlap for f2
 %
-% Dependencies:
+% dependencies:
 % .github/aitchbi/matlab-utils
 % .SPM12
 %
-% Example:
-% sts = hb_nii_verify_overlap(f1,f2,95,'one');
+% examples:
+%   sts = hb_nii_verify_overlap(f1, f2, 95, 'either');
+%   sts = hb_nii_verify_overlap(f1, f2, 95, 'second');
 %
-% HB
+% h behjat
 
 d = inputParser;
 addParameter(d, 'MinimumOverlap', []); % n/a if type=='both'
+addParameter(d, 'AssumeNansAsZeros', false);
 parse(d, varargin{:});
 opts = d.Results;
 
 assert(hb_nii_verify_space_match(f1,f2), 'images are not registered');
 assert(p>1 && p<=100, 'p should be in (1 100]'); 
-v1 = imfill(logical(hb_nii_load(f1)), 'holes');
-v2 = imfill(logical(hb_nii_load(f2)), 'holes');
+
+v1 = imfill_pvt(f1, opts);
+v2 = imfill_pvt(f2, opts);
 vo = and(v1,v2);
 no = nnz(vo);
 p1 = (no/nnz(v1))*100;
@@ -53,7 +57,13 @@ switch type
         if sts
             msg = sprintf('OK. [both have at least %0.1f%% overlap]', p);
         else
-            msg = sprintf('Not OK. [at least one file has less than %0.1f%% overlap]', p);
+            if and(not(c1),not(c2))
+                msg = sprintf('not OK. [both files have less than %0.1f%% overlap with each other]', p);
+            elseif not(c1)
+                msg = sprintf('not OK. [less than %0.1f%% of file 1 overlaps with file 2]', p);
+            else
+                msg = sprintf('not OK. [less than %0.1f%% of file 2 overlaps with file 1]', p);
+            end
         end
 
     case 'either'
@@ -64,14 +74,14 @@ switch type
             if sts
                 msg = ['OK. ', t1];
             else
-                msg = 'Not OK. [neither has overlap above threshold]';
+                msg = 'not OK. [neither has overlap above threshold]';
             end
         else
             pm = opts.MinimumOverlap;
             if p1<pm
                 sts = false;
                 t2 = sprintf('[but file 1 has %0.1f%% overlap with file 2]', p1);
-                msg = sprintf('Not OK. %s %s', t1, t2);
+                msg = sprintf('not OK. %s %s', t1, t2);
             elseif p2<pm
                 sts = false;
                 t2 = sprintf('[but file 2 has %0.1f%% overlap with file 1]', p2);
@@ -92,5 +102,31 @@ switch type
         else
             msg = sprintf('Not OK. [second has %0.1f%% overlap with first]', p2);
         end
+end
+end
+
+%==========================================================================
+function v = imfill_pvt(f, opts)   
+try
+    v = imfill(logical(hb_nii_load(f)), 'holes');
+catch err
+    if contains(err.message, 'NaN')
+        if opts.AssumeNansAsZeros
+            f_tmp = strrep(f, '.nii', '___tmp_hb_nii_verify_overlap.nii');
+            f_tmp = strrep(f_tmp, '.gz', '');
+            [v, h] = hb_nii_load(f);
+            Inan = find(isnan(v));
+            v_tmp = v;
+            h_tmp = h;
+            v_tmp(Inan) = 0;
+            h_tmp.fname = f_tmp; 
+            spm_write_vol(h_tmp, v_tmp);
+            v = imfill(logical(hb_nii_load(f_tmp)), 'holes');
+            delete(f_tmp);
+        end
+    else
+        disp(err);
+        error('hb_nii_verify_space_match: imfill failed');
+    end
 end
 end
