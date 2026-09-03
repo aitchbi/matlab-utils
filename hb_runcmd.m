ab2e031fe85a6435857e5142bbafe5db6fb127f4
hbehjat <hamid.behjat@gmail.com>
2026-09-03 11:29:31 +0200

    Update hb_runcmd.m

--- hb_runcmd.m
+++ hb_runcmd.m
@@ -1,9 +1,12 @@
-function hb_runcmd(cmd,errmsg)
+function t = hb_runcmd(cmd,errmsg)
+tic;
 [sts,log] = system(cmd);
+t = toc;
 if sts==0
     return;
 end
 sprintf('*** system run command log: \n\n');
 log %#ok<NOPRT>
+fprintf('\n.%s [run time: %d minutes]', errmsg, round(t/60));
 error(errmsg)
 end
